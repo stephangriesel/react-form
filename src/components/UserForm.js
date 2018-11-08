3db6de623d75ee8f3a82ec794bcc402591f31484
Stephan Griesel <sgriesel@gmail.com>
2018-11-08 05:12:37 +0100

    add userpersonaldetails

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,60 +1,67 @@
-import React, { Component } from 'react';
-import FormUserDetails from './FormUserDetails';
+import React, { Component } from "react";
+import FormUserDetails from "./FormUserDetails";
+import FormPersonalDetails from "./FormPersonalDetails";
 
 export class UserForm extends Component {
-    state = {
-        step:1,
-        firstName: '',
-        lastName: '',
-        email: '',
-        occupation: '',
-        city: '',
-        bio: ''
-    }
+  state = {
+    step: 1,
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  };
 
-    // Methods: Next Step
-    nextStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        });
-    
-    };
+  // Methods: Next Step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
 
-    // Methods: Previous Step
-    prevStep = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step - 1
-        });
-    };
+  // Methods: Previous Step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
 
-    // Method: Handle 
-    handleChange = input => e => {
-        this.setState({[input]: e.target.value});
-    };
+  // Method: Handle
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
 
   render() {
-      const { step } = this.state;
-      const { firstName, lastName, email, occupation, city, bio } = this.state;
-      const values = { firstName, lastName, email, occupation, city, bio };
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
 
-      switch(step) {
-          case 1: 
-            return (
-              <FormUserDetails 
-              nextStep={this.nextStep}
-              handleChange={this.handleChange}
-              values={values}
-              />
-          );
-          case 2:
-            return <h1>FormPersonalDetails</h1>;
-          case 3:
-            return <h1>Confirm</h1>;
-          case 4:
-            return <h1>Success</h1>;
-      }
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return <h1>Confirm</h1>;
+      case 4:
+        return <h1>Success</h1>;
+    }
   }
 }
 
